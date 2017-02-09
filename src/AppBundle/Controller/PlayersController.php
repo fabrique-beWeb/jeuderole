d089e30f2a03b94a3a018d725b3a26e2fc4b32ee
LoicDerrieux <l.derrieux@fondespierre.com>
2017-02-09 17:22:40 +0100

    Fin pré création de perso (ajout joueur)

--- src/AppBundle/Controller/PlayersController.php
+++ src/AppBundle/Controller/PlayersController.php
@@ -8,10 +8,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Joueur;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Description of PlayersController
@@ -25,12 +26,29 @@ class PlayersController extends Controller{
      * A la fin du traitement on est rediriger sur le controlleur de vue
      * afin de retourner la vue de creation de personnages
      * 
+     * Si le joueur existe en base de donnée, on le met en session
+     * sinon on l'enregistre, et on le met en session.
      * @Route("/players/add",name="addPlayers")
+     * @Method({"POST"})
      * @param \Request $r
      */
     public function addPlayers(Request $r){
-        
-        
+        $entityManager = $this->getDoctrine()->getManager();
+        //boucle sur valeurs de 1 à 4
+        for ($i=1 ; $i<=4 ; $i++){
+            //stockage de la valeur dans la variabe email
+            $email = $r->get('j'. strval($i));
+            if( $email != null){
+                
+                //si nouveau joueur
+                $joueur = new Joueur();
+                $joueur->setEmail($email);
+                $entityManager->persist($joueur);
+                //mise en session du joueur
+                $r->getSession()->set('j'. strval($i), $joueur);                
+            }
+        }
+        $entityManager->flush();
         
         return $this->redirectToRoute('createPerso');
 //        // m'a permis de verifier les valeurs du formulaire
